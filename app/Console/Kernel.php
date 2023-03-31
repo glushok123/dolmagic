a495a119a7c0a668c5b7aa5e9c01eaa3e57768fb
andrey <glushok19999@gmail.com>
2023-03-31 14:40:13 +0300

    Правки

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         Log::info('START CRON');
-        $schedule->command('insales:synchronization')->hourly();
+        $schedule->command('insales:synchronization')->everyThirtyMinutes();
     }
 
     /**
