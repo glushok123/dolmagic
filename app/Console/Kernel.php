db1b7d458766c2752d6ec21caebc24ae6a32ecab
andrey <glushok19999@gmail.com>
2023-04-10 10:05:18 +0300

    Правки

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,16 +16,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        Log::info('##############################################');
         $schedule->command('insales:synchronization')->everyThirtyMinutes();
 
-        Log::info('##############################################');
         $schedule->command('calculate:mrgInterval')->everyTwoHours();
-
-        Log::info('##############################################');
         $schedule->command('calculate:mrgIntervalShort')->everyTenMinutes();
-        
-        Log::info('##############################################');
         $schedule->command('calculate:mrg')->dailyAt('01:00');
     }
 
